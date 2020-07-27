a792f2dcd486811fbbbc18e2b969deb6cfa3516e
Shonuvik <34462179+Shonuvik@users.noreply.github.com>
2020-07-27 16:20:48 -0300

    update title

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
       <BannerMain videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
       url={dadosIniciais.categorias[0].videos[0].url}
-      videoDescription={"O que é FronEnd"} />
+      videoDescription={"God of War - Trailer"} />
 
       <Carousel  
        ignoreFirstVideo
