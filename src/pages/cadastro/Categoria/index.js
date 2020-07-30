7ebea5ba83f5f1bdaae2667b43ff287c962e8059
Shonuvik <34462179+Shonuvik@users.noreply.github.com>
2020-07-30 15:02:33 -0300

    Updating rules

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,7 +30,9 @@ function CadastroCategoria() {
   }
 
     useEffect(() => {
-      const URL = "http://localhost:8080/categorias"
+      const URL = window.location.hostname.includes('localhost') 
+      ? 'http://localhost:8080/categorias'
+      : "https://nfilmxmarlon.herokuapp.com/categorias"
       fetch(URL).then( async(serverResponse)=>{
         if(serverResponse.ok) {
           const response = await serverResponse.json();
